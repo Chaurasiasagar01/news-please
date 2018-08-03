3986f37628c793e2d47ee2961a004f64e595c7f1
Ubuntu <sagaratna@NPUbuntu.lnx4dvzmiwgubf3jbwt2zbwcyg.ix.internal.cloudapp.net>
2018-08-03 06:21:46 +0000

    news-please trial

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ news-please is an open source, easy-to-use news crawler that extracts structured
       zip_safe=False,
       install_requires=[
           'Scrapy>=1.1.0',
-          'PyMySQL>=0.7.9',
+          'psycopg2>=2.7.0',
           'hjson>=1.5.8',
           'elasticsearch>=2.4',
           'beautifulsoup4>=4.3.2',
